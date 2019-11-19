736cb7ba103abfd8a7181c5bc53833c35aa5a3dc
anders_wisur <anders.wisur@gmail.com>
2019-11-20 00:41:01 +0100

    V 4.0

--- src/HttpDispatcher.php
+++ src/HttpDispatcher.php
@@ -52,6 +52,13 @@ class HttpDispatcher {
 
     public function post($url, $params) {
 
+        // Setting required parameters for all requests
+        $params['api_secret'] = $this->config->apiSecret;
+        $params['appID'] = $this->config->appId;
+        $params['t'] = time();
+        $params['v'] = '4.0'; // Version for 
+        $params['s'] = 'php'; // SDK version 
+
         // Removing all 'null' values to reduce parameters sendt.
         foreach ($params as $key => $value) {
             if ($value == null) {
@@ -59,26 +66,20 @@ class HttpDispatcher {
             }
         }
 
-        $params['api_secret'] = $this->config->apiSecret;
-        $params['t'] = time();
-        $params = 'enc_request=' . urlencode($this->_encrypt($params)) . '&appID=' . $this->config->appId . '&v=3.0&s=php';
-
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->config->endpoint . $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-        curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Cert'. DIRECTORY_SEPARATOR .'ip_gd_bundle.crt');
         curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST , 2);
-        curl_setopt($ch, CURLOPT_SSLVERSION, 0 );
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
         $response = new Response();
         $response->rawResponse = curl_exec($ch);
         $response->httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $response->parse();
         curl_close($ch);  //If the session option is not set, close the session.
+
         return $response;
     }
 }
